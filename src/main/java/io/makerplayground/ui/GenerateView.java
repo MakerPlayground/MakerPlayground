bdc6173e1b7293ebd222ce7cb497b9ae342b5b2f
Manchuporn Pungtippimanchai <pinklypop@hotmail.com>
2017-07-20 21:34:25 +0700

    Style Generate Project view

--- src/main/java/io/makerplayground/ui/GenerateView.java
+++ src/main/java/io/makerplayground/ui/GenerateView.java
@@ -1,36 +1,63 @@
 package io.makerplayground.ui;
 
 import io.makerplayground.generator.Diagram;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.stage.Window;
+
+import java.io.IOException;
 
 /**
  * Created by tanyagorn on 7/19/2017.
  */
 public class GenerateView extends Dialog {
+    @FXML private TextArea codeTextArea;
+    @FXML private ScrollPane diagramScrollPane;
+    @FXML private Tab simulateTab;
+    @FXML private Tab codeDeviceTableTab;
+
     private final GenerateViewModel viewModel;
 
     public GenerateView(GenerateViewModel viewModel) {
         this.viewModel = viewModel;
+        Stage stage = (Stage) getDialogPane().getScene().getWindow();
+        stage.initStyle(StageStyle.UTILITY);
+        setTitle("  Generate Project");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/GenerateView.fxml"));
+        fxmlLoader.setRoot(this.getDialogPane());
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         initView();
     }
 
     private void initView() {
         Window window = getDialogPane().getScene().getWindow();
         window.setOnCloseRequest(event -> window.hide());
-        ScrollPane scrollPane = new ScrollPane();
-        VBox test = new VBox();
-
         Diagram wiringDiagram = new Diagram(viewModel.getProject());
-
-        TextArea code = new TextArea();
-        code.setText(viewModel.getCode());
-
-        test.getChildren().addAll(wiringDiagram, code);
-        scrollPane.setContent(test);
-        getDialogPane().setContent(scrollPane);
+        diagramScrollPane.setContent(wiringDiagram);
+        codeTextArea.setText(viewModel.getCode());
+//        ScrollPane scrollPane = new ScrollPane();
+//        VBox test = new VBox();
+//
+//        Diagram wiringDiagram = new Diagram(viewModel.getProject());
+//
+//        TextArea code = new TextArea();
+//        code.setText(viewModel.getCode());
+//        codeTextArea.setText(viewModel.getCode());
+//
+//        test.getChildren().addAll(wiringDiagram, code);
+//        scrollPane.setContent(test);
+//        getDialogPane().setContent(scrollPane);
     }
 }
