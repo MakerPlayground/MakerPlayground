d5444d4ddbaf145edfeb8e74595149c261f14dca
Bright <Bright130@hotmail.com>
2017-09-11 00:10:39 +0700

    update color sensor

--- lib/MP_TCS3200.cpp
+++ lib/MP_TCS3200.cpp
@@ -1,46 +1,94 @@
 
 #include "MP_TCS3200.h"
 
+
+ MP_TCS3200::MP_TCS3200(uint8_t s0,uint8_t s1,uint8_t s2,uint8_t s3,uint8_t out,uint8_t LED)
+ :s0(s0),s1(s1),s2(s2),s3(s3),out(out),LED(LED)
+{
+ 
+}
+
+void rgb2hsv(int r,int g,int b,double out[])
+{
+ 
+
+    double min, max, delta;
+
+    min = r < g ? r : g;
+    min = min  < b ? min  : b;
+
+    max = r > g ? r : g;
+    max = max  > b ? max  : b;
+
+    out[2] = max;                                // v
+    delta = max - min;
+
+
+
+    if (delta < 0.00001)
+    {
+        out[1] = 0;
+        out[0] = 0; // undefined, maybe nan?
+
+
+        return out;
+    }
+    if( max > 0.0 ) { // NOTE: if Max is == 0, this divide would cause a crash
+        out[1] = (delta / max);                  // s
+    } else {
+        // if max is 0, then r = g = b = 0
+        // s = 0, h is undefined
+        out[1] = 0.0;
+        out[0] = NAN ;                            // its now undefined
+        return out;
+    }
+    if( r >= max )                           // > is bogus, just keeps compilor happy
+        out[0] = ( g - b ) / delta;        // between yellow & magenta
+    else
+    if( g >= max )
+        out[0] = 2.0 + ( b - r ) / delta;  // between cyan & yellow
+    else
+        out[0] = 4.0 + ( r - g ) / delta;  // between magenta & cyan
+
+
+    out[0] *= 60.0;                              // degrees
+
+    if( out[0] < 0.0 )
+        out[0] += 360.0;
+
+   
+    return out;
+}
+
+
 void MP_TCS3200::init() 
 {
- int outputEnabled = 2; // write LOW to turn on Note, may not be hooked up.
- int s0 = 3; // sensor pins
- int s1 = 4;
- int s2 = 5;
- int s3 = 6;
- int nLED = 7; // illuminating LED
- int out = 8; // TCS230 output
 
-pinMode(outputEnabled, OUTPUT);
+
 pinMode(s0, OUTPUT);
 pinMode(s1, OUTPUT);
 pinMode(s2, OUTPUT);
 pinMode(s3, OUTPUT);
-pinMode(nLED, OUTPUT);
+pinMode(LED, OUTPUT);
 pinMode(out, INPUT);
 //This pin may be set to ground and not available on the breakout
 //If not available don't worry about it.
-digitalWrite(outputEnabled, LOW);
+
 //Set Frequency scaling to largest value
 digitalWrite(s0, HIGH);
 digitalWrite(s1, HIGH);
-digitalWrite(nLED, LOW);
+digitalWrite(LED, LOW);
 }
 
 int MP_TCS3200::isColor(char color[]) 
 
 {
-   int outputEnabled = 2; // write LOW to turn on Note, may not be hooked up.
- int s0 = 3; // sensor pins
- int s1 = 4;
- int s2 = 5;
- int s3 = 6;
- int nLED = 7; // illuminating LED
- int out = 8; // TCS230 output
+  delay(100);
+
   int red = 0;
 int green = 0;
 int blue = 0;
-digitalWrite(nLED,1);
+digitalWrite(LED,1);
 digitalWrite(s2, LOW);
 digitalWrite(s3, LOW);
 // count OUT, pRed, RED
@@ -51,41 +99,83 @@ blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
 digitalWrite(s2, HIGH);
 // count OUT, pGreen, GREEN
 green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-digitalWrite(nLED,0);
+digitalWrite(LED,0);
 Serial.print("R");
-Serial.print(red, DEC);
+Serial.print(255-red, DEC);
 Serial.print(" G");
-Serial.print(green, DEC);
+Serial.print(255-green, DEC);
 Serial.print(" B");
-Serial.print(blue, DEC);
+Serial.print(255-blue, DEC);
 Serial.println();
-//Simple logic to test for color
-if (red < blue && red < green) {
-  if(strcmp(color, "Red") == 0)
-    return 1;
-  else
-    return 0;
-}
-else if (blue < red && blue < green) 
-  {
-  if(strcmp(color, "Blue") == 0)
-    return 1;
-  else
-    return 0;
-}
-else 
-  {
-  if(strcmp(color, "Green") == 0)
-    return 1;
-  else
-    return 0;
-}
 
+double hsv[3] ;
+rgb2hsv(255-red,255-green,255-blue,hsv);
+hsv[1]*=100;
+hsv[2]*=100;
+hsv[2]/=255;
 
+   Serial.println(hsv[0]) ;
+Serial.println(hsv[1]) ;
+      Serial.println(hsv[2]) ;
+Serial.println("-----") ;
 
-
-}
-
- MP_TCS3200::MP_TCS3200()
+if(hsv[2]<20)
 {
- }
+  Serial.println("Black") ;
+  return 1;
+}
+else if(hsv[2]>90&&hsv[1]<5)
+{
+   Serial.println("White") ;
+  return 1;
+}
+
+else if((hsv[0]>=330 || hsv[0]<15))
+{
+   Serial.println("Red") ;
+  return 1;
+}
+else if((hsv[0]>=15 && hsv[0]<25))
+{
+   Serial.println("Orange") ;
+  return 1;
+}
+else if((hsv[0]>=25 && hsv[0]<75)) //30
+{
+   Serial.println("Yellow") ;
+  return 1;
+}
+else if((hsv[0]>=75 && hsv[0]<165))//135
+{
+   Serial.println("Green") ;
+  return 1;
+}
+else if((hsv[0]>=165 && hsv[0]<190)) //210
+{
+   Serial.println("Cyan") ;
+  return 1;
+}
+else if((hsv[0]>=190 && hsv[0]<235)) //225
+{
+   Serial.println("Blue") ;
+  return 1;
+}
+else if((hsv[0]>=235 && hsv[0]<270))
+{
+   Serial.println("Violet") ;
+  return 1;
+}
+else if((hsv[0]>=270 && hsv[0]<330))
+{
+   Serial.println("Margenta") ;
+  return 1;
+}
+
+
+
+
+
+}
+
+
+
--- lib/MP_TCS3200.h
+++ lib/MP_TCS3200.h
@@ -1,18 +1,25 @@
 #ifndef MP_TCS3200_H
 #define MP_TCS3200_H
-#define commonAnode true
 
-//#include "MP_Button.h"
+
 #include <Arduino.h>
 #include <Wire.h>
 
 class MP_TCS3200 //: MP_Button
 {
   public:
-	 MP_TCS3200() ;
+	 MP_TCS3200(uint8_t s0,uint8_t s1,uint8_t s2,uint8_t s3,uint8_t out,uint8_t LED) ;
 	 ~MP_TCS3200() {};
 	 int isColor(char color[]);
 	  void init() ;
+
+	private:
+		uint8_t s0 ;
+		uint8_t s1 ;
+		uint8_t s2 ;
+		uint8_t s3 ;
+		uint8_t out ;
+		uint8_t LED ;
 
 	  
 };
