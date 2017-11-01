d3f02ba680f4688e13d5d2d0df84b815e5bf8fe3
Bright <Bright130@hotmail.com>
2017-11-01 19:59:36 +0700

    Add LDR and fixed MPU6050

--- upload/project/src/main.cpp
+++ upload/project/src/main.cpp
@@ -1,22 +1,16 @@
 #include <Arduino.h>
-#include "MP_LED.h"
-#include "MP_BME280.h"
-#include "MP_LSM9DS1.h"
+#include "MP_OneAnalogReadToPercent.h"
 
 void beginScene();
 void scene1();
 
 void (*currentScene)(void);
 
-MP_LED LED1(3);
-MP_BME280 Temperature1;
-MP_LSM9DS1 Accelerometer1;
+MP_OneAnalogReadToPercent _Light1(A5);
 
 void setup() {
     Serial.begin(115200);
-    LED1.init();
-    Temperature1.init();
-    Accelerometer1.init();
+    _Light1.init();
     currentScene = beginScene;
 }
 
@@ -25,10 +19,10 @@ void loop() {
 }
 
 void beginScene() {
-    double Accelerometer1_Accel_X;
+    double _Light1_Light;
     while (1) {
-        Accelerometer1_Accel_X = Accelerometer1.getAccel_X();
-        if((Accelerometer1_Accel_X < 78.78787878787881 && Accelerometer1_Accel_X > -65.07936507936506)) {
+        _Light1_Light = _Light1.getLight();
+        if((Light1_Light < 75.0 && Light1_Light > 25.0)) {
             currentScene = scene1;
             break;
         }
@@ -36,6 +30,5 @@ void beginScene() {
 }
 
 void scene1() {
-    LED1.on(50.00);
     currentScene = beginScene;
 }
