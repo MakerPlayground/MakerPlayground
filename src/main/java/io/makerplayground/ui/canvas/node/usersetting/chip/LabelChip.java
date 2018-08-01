742f5b7c55e16accb70b7205aa4bcd5e9059ab78
Chattriya Jariyavajee <jetstreamc@users.noreply.github.com>
2018-08-02 01:32:25 +0700

    refactor ChipField: separate fxml and css

--- src/main/java/io/makerplayground/ui/canvas/node/usersetting/chip/LabelChip.java
+++ src/main/java/io/makerplayground/ui/canvas/node/usersetting/chip/LabelChip.java
@@ -1,16 +1,18 @@
 package io.makerplayground.ui.canvas.node.usersetting.chip;
 
 import io.makerplayground.project.term.Term;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
+
 public class LabelChip extends Chip<String> {
 
-//    private static final Color BACKGROUND_COLOR = Color.DARKRED;
-    private static final Color BACKGROUND_COLOR = Color.valueOf("081e42");
-//    private static final Color BACKGROUND_COLOR_SELECTED = Color.RED;
-
+    @FXML private Rectangle background;
+    @FXML private Text text;
     public LabelChip(String msg) {
         this(msg, Term.Type.STRING);
     }
@@ -21,16 +23,17 @@ public class LabelChip extends Chip<String> {
 
     @Override
     protected void initView() {
-        Rectangle background = new Rectangle();
-        background.setWidth(80);
-        background.setHeight(30);
-        background.setArcWidth(20);
-        background.setArcHeight(20);
-        background.fillProperty().setValue(BACKGROUND_COLOR);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/canvas/node/usersetting/chip/Labelchip.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
 
-        Text text = new Text(getValue());
-        text.setFill(Color.WHITE);
-        getChildren().addAll(background, text);
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        text.setText(getValue());
     }
 
     @Override
