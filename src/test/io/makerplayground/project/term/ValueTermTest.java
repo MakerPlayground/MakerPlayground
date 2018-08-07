32173a2b99900d18d229d80135036afe43590925
Unknown <chattriya.j@mail.kmutt.ac.th>
2018-08-07 13:24:22 +0700

    fix and add testcase for ValueTermTest

--- src/test/io/makerplayground/project/term/ValueTermTest.java
+++ src/test/io/makerplayground/project/term/ValueTermTest.java
@@ -1,24 +1,41 @@
 package io.makerplayground.project.term;
 
+import io.makerplayground.device.DeviceLibrary;
+import io.makerplayground.project.Project;
+import io.makerplayground.project.ProjectDevice;
 import io.makerplayground.project.ProjectValue;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValueTermTest {
     private ValueTerm tester;
+    private ProjectValue projectValue;
 
-    //@BeforeEach
-    //void setTester(){ tester = new ValueTerm(new ProjectValue("",""));}
+    ValueTermTest() {
+        DeviceLibrary library = DeviceLibrary.INSTANCE;
+        library.loadDeviceFromJSON();
+        Project project = new Project();
+        project.addSensor(library.getGenericDevice("Accelerometer"));
+        projectValue = project.getAvailableValue().get(0);
+        tester = new ValueTerm(projectValue);
+    }
 
     @Test
-    void isValidTest() {
-        assertNotNull(tester);
+    void isValidTestMustBeTrueIfProjectValueIsNotNull() {
+        assertTrue(tester.isValid());
+    }
+
+    @Test
+    void isValidMustBeFalseIfProjectValueIsNull() {
+        ValueTerm testerNull;
+        testerNull = new ValueTerm(null);
+        assertFalse(testerNull.isValid());
     }
 
     @Test
     void ValueToCCode() {
-        assertEquals("",tester.toCCode());
+        String expectingName = String.format("_%s_%s", projectValue.getDevice().getName(), projectValue.getValue().getName());
+        assertEquals(expectingName, tester.toCCode());
     }
 }
