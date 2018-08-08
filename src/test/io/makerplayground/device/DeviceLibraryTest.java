20c2574695944b5a06d44836c41565896a0bcb7f
Unknown <chattriya.j@mail.kmutt.ac.th>
2018-08-08 23:54:08 +0700

    add DeviceLibraryTest

---
+++ src/test/io/makerplayground/device/DeviceLibraryTest.java
@@ -0,0 +1,52 @@
+package io.makerplayground.device;
+
+import io.makerplayground.helper.Platform;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeviceLibraryTest {
+
+    private DeviceLibrary library = DeviceLibrary.INSTANCE;
+
+    DeviceLibraryTest() {
+        library.loadDeviceFromJSON();
+    }
+
+    @Test
+    void getGenericDevice() {
+        assertNotNull(library.getGenericDevice("LED"));
+        assertNull(library.getGenericDevice(""));
+    }
+
+    @Test
+    void getGenericInputDevice() {
+        assertNotNull(library.getGenericInputDevice());
+        assertFalse(library.getGenericInputDevice().isEmpty());
+    }
+
+    @Test
+    void getGenericOutputDevice() {
+        assertNotNull(library.getGenericOutputDevice());
+        assertFalse(library.getGenericOutputDevice().isEmpty());
+    }
+
+    @Test
+    void getGenericConnectivityDevice() {
+        assertNotNull(library.getGenericConnectivityDevice());
+        assertFalse(library.getGenericConnectivityDevice().isEmpty());
+    }
+
+    @Test
+    void getActualDevice() {
+        assertNotNull(library.getActualDevice());
+        assertFalse(library.getActualDevice().isEmpty());
+
+        assertNotNull(library.getActualDevice("MP-0000"));
+        assertNull(library.getActualDevice(""));
+
+        assertNotNull(library.getActualDevice(Platform.MP_ARDUINO));
+        assertFalse(library.getActualDevice(Platform.MP_ARDUINO).isEmpty());
+    }
+}
