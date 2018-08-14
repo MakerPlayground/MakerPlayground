2d8d5fb857eee0d5d44bcb2a5a18771731920081
Budsapanee Pongsiriyaporn <budsapanee@gmail.com>
2018-08-14 22:57:39 +0700

    change the overall style of the program to light theme

--- src/main/java/io/makerplayground/ui/canvas/node/InteractiveNode.java
+++ src/main/java/io/makerplayground/ui/canvas/node/InteractiveNode.java
@@ -100,11 +100,14 @@ public abstract class InteractiveNode extends Group implements Selectable {
      */
     protected void showHilight(boolean forceHilight) {
         if (forceHilight || isSelected()) {
-            setStyle("-fx-effect: dropshadow(gaussian, #5ac2ab, 15.0 , 0.5, 0.0 , 0.0);");
+            setStyle("-fx-effect: dropshadow(gaussian, #008ef4, 5.0 , 0.5, 0.0 , 0.0);");
+            /*setStyle("-fx-effect: dropshadow(gaussian, #5ac2ab, 15.0 , 0.5, 0.0 , 0.0);");*/
         } else if (isError()) {
-            setStyle("-fx-effect: dropshadow(gaussian, #c25a5a, 15.0 , 0.5, 0.0 , 0.0);");
+            setStyle("-fx-effect: dropshadow(gaussian, #ff8b01, 5.0 , 0.5, 0.0 , 0.0);");
+            /*setStyle("-fx-effect: dropshadow(gaussian, #c25a5a, 15.0 , 0.5, 0.0 , 0.0);");*/
         } else {
-            setStyle("-fx-effect: dropshadow(gaussian, derive(black,75%), 15.0 , 0.0, 0.0 , 0.0);");
+            setStyle("-fx-effect: dropshadow(gaussian, derive(black,85%), 5.0 , 0.0, 0.0 , 0.0);");
+            /*setStyle("-fx-effect: dropshadow(gaussian, derive(black,75%), 15.0 , 0.0, 0.0 , 0.0);");*/
         }
     }
 
