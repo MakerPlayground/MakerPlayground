8621e3d30cb86330d05e5e626f66fb5d91f90c19
Budsapanee Pongsiriyaporn <budsapanee@gmail.com>
2018-08-28 11:43:22 +0700

    Change chip color

--- src/main/java/io/makerplayground/ui/canvas/node/usersetting/chip/ProjectValueChip.java
+++ src/main/java/io/makerplayground/ui/canvas/node/usersetting/chip/ProjectValueChip.java
@@ -89,9 +89,9 @@ public class ProjectValueChip extends Chip<ProjectValue> {
     @Override
     protected void updateChipStyle(boolean selected) {
         if (!selected) {
-            background.setFill(Color.web("#081949"));
+            background.setFill(Color.web("#9129d7"));
         } else {
-            background.setFill(Color.web("#081949").darker());
+            background.setFill(Color.web("#9129d7").darker());
         }
     }
 }
