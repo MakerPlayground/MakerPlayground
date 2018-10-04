d7d502a08f82cfea1d30882efc8de4ac90efe12a
Chattriya Jariyavajee <chattriya.j@mail.kmutt.ac.th>
2018-10-04 17:16:48 +0700

    add abstract lib name to each cloud platform

--- src/main/java/io/makerplayground/device/CloudPlatform.java
+++ src/main/java/io/makerplayground/device/CloudPlatform.java
@@ -3,14 +3,17 @@ package io.makerplayground.device;
 import java.util.List;
 
 public enum CloudPlatform {
-    BLYNK("Blynk", List.of("Auth Key", "Wifi's SSID", "Wifi's Password")), NETPIE("Netpie", List.of("App Id", "Key", "Secret", "Alias", "Wifi's SSID", "Wifi's Password"));
+    BLYNK("Blynk", List.of("Auth Key", "Wifi's SSID", "Wifi's Password"), "MP_BLYNK"),
+    NETPIE("Netpie", List.of("App Id", "Key", "Secret", "Alias", "Wifi's SSID", "Wifi's Password"), ""); // TODO: Add netpie
 
     private final String displayName;
     private final List<String> parameter;
+    private final String libName;
 
-    CloudPlatform(String displayName, List<String> parameter) {
+    CloudPlatform(String displayName, List<String> parameter, String libName) {
         this.displayName = displayName;
         this.parameter = parameter;
+        this.libName = libName;
     }
 
     public String getDisplayName() {
@@ -19,5 +22,9 @@ public enum CloudPlatform {
 
     public List<String> getParameter() {
         return parameter;
+    }
+
+    public String getLibName() {
+        return libName;
     }
 }
