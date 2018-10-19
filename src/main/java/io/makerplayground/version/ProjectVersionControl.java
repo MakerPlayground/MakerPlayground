c4bcd50464f6f7ed3efb5039370a7464c068822a
Chattriya Jariyavajee <jetstreamc@users.noreply.github.com>
2018-10-19 20:29:35 +0700

    update project file version

--- src/main/java/io/makerplayground/version/ProjectVersionControl.java
+++ src/main/java/io/makerplayground/version/ProjectVersionControl.java
@@ -21,16 +21,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class ProjectVersionControl {
 
-    public static final String CURRENT_VERSION = "0.2.3";
+    public static final String CURRENT_VERSION = "0.3.0";
 
     public static boolean isConvertibleToCurrentVersion(String projectVersion) {
+        List<String> incompatibleList = List.of("0.2", "0.2.3");
         if (CURRENT_VERSION.equals(projectVersion)) {
             return true;
         }
-        else if ("0.2".equals(projectVersion)) {
+        else if (incompatibleList.contains(projectVersion)) {
             return false;
         }
         return false;
