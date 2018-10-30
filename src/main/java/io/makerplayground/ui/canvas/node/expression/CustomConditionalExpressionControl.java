2c65a0858e52e7f211b206a298e99db72156f59a
Nuntipat Narkthong <nuntipat@gmail.com>
2018-10-31 00:02:22 +0700

    Add label to indicate that each entries of the CustomConditionalExpression are 'AND' together and prevent the last entry from being removed

--- src/main/java/io/makerplayground/ui/canvas/node/expression/CustomConditionalExpressionControl.java
+++ src/main/java/io/makerplayground/ui/canvas/node/expression/CustomConditionalExpressionControl.java
@@ -6,29 +6,28 @@ import io.makerplayground.project.expression.CustomNumberExpression;
 import io.makerplayground.project.term.Operator;
 import io.makerplayground.ui.canvas.node.expression.custom.ChipField;
 import javafx.collections.FXCollections;
-import javafx.geometry.HPos;
-import javafx.geometry.VPos;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CustomConditionalExpressionControl extends GridPane {
+public class CustomConditionalExpressionControl extends VBox {
 
     private final ConditionalExpression expression;
     private final List<ProjectValue> projectValues;
-
-    private int currentRow = 0;
+    private final List<EntryView> entryViewList;
 
     public CustomConditionalExpressionControl(ConditionalExpression expression, List<ProjectValue> projectValues) {
         this.expression = expression;
         this.projectValues = projectValues;
-
-        for (ConditionalExpression.Entry entry : expression.getEntries()) {
-            createExpressionRowControl(entry);
-        }
+        this.entryViewList = new ArrayList<>();
 
         Button addButton = new Button("+");
         addButton.setMaxWidth(Double.MAX_VALUE);
@@ -36,48 +35,83 @@ public class CustomConditionalExpressionControl extends GridPane {
             ConditionalExpression.Entry entry = new ConditionalExpression.Entry(Operator.GREATER_THAN, new CustomNumberExpression());
             expression.getEntries().add(entry);
             createExpressionRowControl(entry);
-            GridPane.setRowIndex(addButton, currentRow);
-            currentRow++;
         });
-        GridPane.setConstraints(addButton, 0, currentRow, 2, 1);
         getChildren().add(addButton);
-        currentRow++;
 
-        setHgap(5);
+        for (ConditionalExpression.Entry entry : expression.getEntries()) {
+            createExpressionRowControl(entry);
+        }
+
+        setSpacing(5);
     }
 
-
     private void createExpressionRowControl(ConditionalExpression.Entry entry) {
-        ComboBox<Operator> operatorComboBox = new ComboBox<>(FXCollections.observableArrayList(Operator.getComparisonOperator()));
-        if (entry.getOperator() != null) {
-            operatorComboBox.setValue(entry.getOperator());
+        EntryView entryView = new EntryView(entry, projectValues);
+        entryView.setOnRemoveButtonPressed(event -> {
+            expression.getEntries().remove(entry);
+            getChildren().remove(entryView);
+            entryViewList.remove(entryView);
+            invalidateView();
+        });
+        getChildren().add(getChildren().size() - 1, entryView);
+        entryViewList.add(entryView);
+        invalidateView();
+    }
+
+    private void invalidateView() {
+        for (EntryView view : entryViewList) {
+            view.setOperatorVisible(true);
+            view.setRemoveButtonVisible(true);
         }
-        operatorComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
-            entry.setOperator(newValue);
-        });
-        GridPane.setConstraints(operatorComboBox, 0, currentRow, 1, 1, HPos.CENTER, VPos.TOP);
+        // hide 'and' label of the last expression entry
+        entryViewList.get(entryViewList.size() - 1).setOperatorVisible(false);
+        // hide remove button when there is only 1 entry in the expression
+        if (entryViewList.size() == 1) {
+            entryViewList.get(0).setRemoveButtonVisible(false);
+        }
+    }
 
-        ChipField chipField = new ChipField(entry.getExpression(), projectValues);
-        chipField.setRefreshIntervalAdjustable(false);
-        chipField.expressionProperty().addListener((observable, oldValue, newValue) -> entry.setExpression(newValue));
-        GridPane.setConstraints(chipField, 1, currentRow);
+    private static class EntryView extends HBox {
 
-        // we add this node to reserve some spaces in the next row and remove it when this row is removed because
-        // the vgap is also applied to the empty row emerged after we remove some rows in the middle (updating the
-        // constraint of every row behind is hard so we do it this way)
-        Pane spring = new Pane();
-        spring.setMinHeight(5);
-        GridPane.setConstraints(spring, 0, currentRow+1);
+        private Button removeButton;
+        private Label operatorLabel;
 
-        Button removeButton = new Button("-");
-        removeButton.setOnAction(event -> {
-            getChildren().removeAll(operatorComboBox, chipField, removeButton, spring);
-            getExpression().getEntries().remove(entry);
-        });
-        GridPane.setConstraints(removeButton, 2, currentRow, 1, 1, HPos.CENTER, VPos.TOP);
+        public EntryView(ConditionalExpression.Entry entry, List<ProjectValue> projectValues) {
+            ComboBox<Operator> operatorComboBox = new ComboBox<>(FXCollections.observableArrayList(Operator.getComparisonOperator()));
+            if (entry.getOperator() != null) {
+                operatorComboBox.setValue(entry.getOperator());
+            }
+            operatorComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+                entry.setOperator(newValue);
+            });
 
-        getChildren().addAll(operatorComboBox, chipField, removeButton, spring);
-        currentRow += 2;
+            ChipField chipField = new ChipField(entry.getExpression(), projectValues);
+            chipField.setRefreshIntervalAdjustable(false);
+            chipField.expressionProperty().addListener((observable, oldValue, newValue) -> entry.setExpression(newValue));
+
+            operatorLabel = new Label("and");
+            operatorLabel.setMinHeight(25); // a hack to center the label to the height of 1 row control when the control spans to multiple rows
+            operatorLabel.managedProperty().bind(operatorLabel.visibleProperty());
+
+            removeButton = new Button("-");
+            removeButton.managedProperty().bind(removeButton.visibleProperty());
+
+            setAlignment(Pos.TOP_LEFT);
+            setSpacing(5);
+            getChildren().addAll(operatorComboBox, chipField, removeButton, operatorLabel);
+        }
+
+        void setOnRemoveButtonPressed(EventHandler<ActionEvent> e) {
+            removeButton.setOnAction(e);
+        }
+
+        void setRemoveButtonVisible(boolean b) {
+            removeButton.setVisible(b);
+        }
+
+        void setOperatorVisible(boolean b) {
+            operatorLabel.setVisible(b);
+        }
     }
 
     public ConditionalExpression getExpression() {
