2af92ddf842d12a1ed471601684c2209feac9e10
Nuntipat Narkthong <nuntipat@gmail.com>
2018-12-27 16:36:54 +0700

    Refactor UploadTask and revise upload progress indication

--- src/main/java/io/makerplayground/generator/upload/UploadTask.java
+++ src/main/java/io/makerplayground/generator/upload/UploadTask.java
@@ -43,7 +43,6 @@ public class UploadTask extends Task<UploadResult> {
         updateProgress(0, 1);
         updateMessage("Checking project");
 
-        /* Device Mapping */
         DeviceMapperResult mappingResult = DeviceMapper.autoAssignDevices(project);
         if (mappingResult == DeviceMapperResult.NOT_ENOUGH_PORT) {
             updateMessage("Error: not enough port available");
@@ -59,18 +58,18 @@ public class UploadTask extends Task<UploadResult> {
             return UploadResult.UNKNOWN_ERROR;
         }
 
-        /* Code Generating */
         SourceCodeResult sourcecode = SourceCodeGenerator.generateCode(project, true);
         if (sourcecode.getError() != null) {
             updateMessage("Error: " + sourcecode.getError().getDescription());
             return UploadResult.CANT_GENERATE_CODE;
         }
 
-        updateProgress(0.15, 1);
+        updateProgress(0.10, 1);
+        updateMessage("Checking required dependencies");
 
         Platform.runLater(() -> log.set("Install directory is at " + MP_INSTALLDIR + "\n"));
 
-        /* check platformio installation */
+        // check platformio installation
         Optional<String> pythonPath = getPythonPath();
         if (!pythonPath.isPresent()) {
             updateMessage("Error: Can't find python with valid platformio installation see: http://docs.platformio.org/en/latest/installation.html");
@@ -78,6 +77,7 @@ public class UploadTask extends Task<UploadResult> {
         }
         Platform.runLater(() -> log.set("Using python at " + pythonPath.get() + "\n"));
 
+        // check platformio home directory
         Optional<String> pioHomeDirPath = getIntegratedPIOHomeDirectory();
         if (pioHomeDirPath.isPresent()) {
             Platform.runLater(() -> log.set("Using integrated platformio dependencies at " + pioHomeDirPath.get() + "\n"));
@@ -85,10 +85,8 @@ public class UploadTask extends Task<UploadResult> {
             Platform.runLater(() -> log.set("Using default platformio dependencies folder (~/.platformio) \n"));
         }
 
-        /* Library List Preparing */
-        updateProgress(0.25, 1);
+        updateProgress(0.20, 1);
         updateMessage("Preparing to generate project");
-        String code = sourcecode.getCode();
 
         List<ActualDevice> actualDevicesUsed = project.getAllDeviceUsed().stream()
                 .map(ProjectDevice::getActualDevice)
@@ -135,61 +133,33 @@ public class UploadTask extends Task<UploadResult> {
             Platform.runLater(() -> log.set(" - " + libName + "\n"));
         }
 
-        /* Source Files Preparing */
-        updateProgress(0.3, 1);
-        updateMessage("Preparing platformio project");
+        updateMessage("Generating project");
         String projectPath = MP_WORKSPACE + File.separator + "upload";
         Platform.runLater(() -> log.set("Generating project at " + projectPath + "\n"));
-        Process p = null;
         try {
             FileUtils.deleteDirectory(new File(projectPath));
             FileUtils.forceMkdir(new File(projectPath));
-
-            ProcessBuilder builder = new ProcessBuilder(pythonPath.get(), "-m", "platformio", "init"
-                    , "--board", project.getController().getPlatformIOBoardId());
-            builder.directory(new File(projectPath).getAbsoluteFile()); // this is where you set the root folder for the executable to run with
-            pioHomeDirPath.ifPresent(s -> builder.environment().put("PLATFORMIO_HOME_DIR", s));
-            builder.redirectErrorStream(true);
-            p = builder.start();
-            try (Scanner s = new Scanner(p.getInputStream())) {
-                while (s.hasNextLine()) {
-                    if (isCancelled()) {
-                        throw new InterruptedException();
-                    }
-                    String line = s.nextLine();
-                    Platform.runLater(() -> log.set(line + "\n"));
-                }
-            }
-            if (p.waitFor() != 0) {
-                updateMessage("Error: pio init failed");
-                return UploadResult.UNKNOWN_ERROR;
-            }
-        } catch (InterruptedException e) {
-            if (isCancelled()) {
-                updateMessage("Canceling upload...");
-                killProcess(p);
-                updateMessage("Upload has been canceled");
-                return UploadResult.USER_CANCEL;
-            }
-            updateMessage("Unknown error has occurred. Please try again.");
-            return UploadResult.UNKNOWN_ERROR;
         } catch (IOException e) {
             updateMessage("Error: can't create project directory (permission denied)");
-            return UploadResult.NO_PERMISSION;
+            return UploadResult.CANT_CREATE_PROJECT;
+        }
+        UploadResult result = runPlatformIOCommand(pythonPath.get(), projectPath, pioHomeDirPath
+                , List.of("init", "--board", project.getController().getPlatformIOBoardId())
+                , "Error: Can't create project directory (permission denied)", UploadResult.CANT_CREATE_PROJECT);
+        if (result != UploadResult.OK) {
+            return result;
         }
 
-        updateProgress(0.5, 1);
-        updateMessage("Generate source files and libraries");
+        updateProgress(0.4, 1);
+        updateMessage("Generating source files and libraries");
         try {
             FileUtils.forceMkdir(new File(projectPath + File.separator + "src"));
             FileUtils.forceMkdir(new File(projectPath + File.separator + "lib"));
 
             // generate source file
-            FileWriter fw = new FileWriter(projectPath + File.separator + "src" + File.separator + "main.cpp");
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(code);
-            bw.close();
-            fw.close();
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(projectPath + File.separator + "src" + File.separator + "main.cpp"))){
+                bw.write(sourcecode.getCode());
+            }
         } catch (IOException | NullPointerException e) {
             updateMessage("Error: Cannot write code to project directory");
             return UploadResult.CANT_WRITE_CODE;
@@ -228,10 +198,37 @@ public class UploadTask extends Task<UploadResult> {
             }
         }
 
-        updateProgress(0.75, 1);
+        updateProgress(0.6, 1);
+        updateMessage("Building project");
+        result = runPlatformIOCommand(pythonPath.get(), projectPath, pioHomeDirPath, List.of("run"),
+                "Error: Can't build the generated sourcecode. Please contact the development team.", UploadResult.CODE_ERROR);
+        if (result != UploadResult.OK) {
+            return result;
+        }
+
+        updateProgress(0.8, 1);
         updateMessage("Uploading to board");
+        result = runPlatformIOCommand(pythonPath.get(), projectPath, pioHomeDirPath, List.of("run", "--target", "upload"),
+                "Error: Can't find board. Please check connection.", UploadResult.CANT_FIND_BOARD);
+        if (result != UploadResult.OK) {
+            return result;
+        }
+
+        updateProgress(1, 1);
+        updateMessage("Done");
+
+        return UploadResult.OK;
+    }
+
+    private UploadResult runPlatformIOCommand(String pythonPath, String projectPath, Optional<String> pioHomeDirPath, List<String> args
+            , String errorMessage, UploadResult error) {
+        Process p = null;
         try {
-            ProcessBuilder builder = new ProcessBuilder(pythonPath.get(), "-m", "platformio", "run", "--target", "upload");
+            // create argument list
+            List<String> arguments = new ArrayList<>(List.of(pythonPath, "-m", "platformio"));
+            arguments.addAll(args);
+            // create process to invoke platformio
+            ProcessBuilder builder = new ProcessBuilder(arguments);
             builder.directory(new File(projectPath).getAbsoluteFile()); // this is where you set the root folder for the executable to run with
             pioHomeDirPath.ifPresent(s -> builder.environment().put("PLATFORMIO_HOME_DIR", s));
             builder.redirectErrorStream(true);
@@ -247,8 +244,8 @@ public class UploadTask extends Task<UploadResult> {
             }
             int result = p.waitFor();
             if (result != 0) {
-                updateMessage("Error: Can't find board. Please check connection.");
-                return UploadResult.CANT_FIND_BOARD;
+                updateMessage(errorMessage);
+                return error;
             }
         } catch (InterruptedException e) {
             if (isCancelled()) {
@@ -260,26 +257,21 @@ public class UploadTask extends Task<UploadResult> {
             updateMessage("Unknown error has occurred. Please try again.");
             return UploadResult.UNKNOWN_ERROR;
         } catch (IOException e) {
-            updateMessage("Error: can't find project directory (permission denied)");
-            return UploadResult.NO_PERMISSION;
+            updateMessage("Unknown error (I/O) has occurred. Please try again.");
+            return UploadResult.UNKNOWN_ERROR;
         }
-
-        updateProgress(1, 1);
-        updateMessage("Done");
-
         return UploadResult.OK;
     }
+
 
     private void killProcess(Process p) {
         try {
             if (OSInfo.getOs() == OSInfo.OS.WINDOWS) {
-                Process killerProcess = Runtime.getRuntime().exec("taskkill /f /t /pid " + p.pid());
-                killerProcess.waitFor();
+                Runtime.getRuntime().exec("taskkill /f /t /pid " + p.pid());
             } else {
                 p.destroy();
             }
-            p.waitFor();
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
