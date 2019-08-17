01be1c8d867fa32eaf9ee136cf2656a36c7f85b3
Nuntipat Narkthong <nuntipat@gmail.com>
2019-08-17 13:54:47 +0700

    Update version of dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ open module io.makerplayground.frontend {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
-    requires controlsfx;
+    requires org.controlsfx.controls;
     requires com.fazecast.jSerialComm;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
