db0656bb1121b24844f31280004f80d1ff4694a3
Chattriya Jariyavajee <jetstreamc@users.noreply.github.com>
2019-10-12 20:40:37 +0700

    Update Copyright

--- src/main/java/io/makerplayground/version/ProjectVersionControl.java
+++ src/main/java/io/makerplayground/version/ProjectVersionControl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018. The Maker Playground Authors.
+ * Copyright (c) 2019. The Maker Playground Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
