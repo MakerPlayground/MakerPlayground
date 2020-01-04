6e6af56f558b1effa81197b506c7076196a73ebb
Nuntipat Narkthong <nuntipat@gmail.com>
2020-01-04 21:27:52 +0700

    Use json format in the version file, update version checking URL and add support for stable, nightly and internal release channels

--- src/main/java/io/makerplayground/version/SoftwareVersion.java
+++ src/main/java/io/makerplayground/version/SoftwareVersion.java
@@ -21,46 +21,34 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.scene.control.Alert;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.URL;
 import java.net.UnknownHostException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 public class SoftwareVersion implements Comparable<SoftwareVersion> {
     private static SoftwareVersion currentVersion;
-    private static final String URL = "http://mprepo.azurewebsites.net/current_version"; // or "http://mprepo.azurewebsites.net/devtest/current_version"
+    private static final String URL = "https://makerplayground.z23.web.core.windows.net/version";
 
     public static SoftwareVersion getCurrentVersion() {
         if (currentVersion == null) {
-            try {
-                Path path = Paths.get("version.txt");
-                // TODO: it may be better to return an optional and alert user outside of this method but this error is rarely happen so it is ok
-                if (!Files.exists(path)) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setContentText("Can't read version information from " + path.toAbsolutePath().toString());
-                    alert.showAndWait();
-                } else {
-                    List<String> versionFile = Files.readAllLines(Paths.get("version.txt"));
-                    if (versionFile.size() == 2) {
-                        String[] version = versionFile.get(0).split("=");
-                        String[] releaseDate = versionFile.get(1).split("=");
-                        if (version.length == 2 && version[0].equals("version")
-                                && releaseDate.length == 2 && releaseDate[0].equals("release-date")) {
-                            currentVersion = new SoftwareVersion("Maker Playground " + version[1], version[1], "http://makerplayground.io"
-                                    , new Date(LocalDate.parse(releaseDate[1], DateTimeFormatter.ISO_LOCAL_DATE).toEpochDay()));
-                        }
-                    }
+            File file = new File("version.json");
+            if (!file.exists()) {
+                // it may be better to return an optional and alert user outside of this method but this error should
+                // only happen during development so it is ok
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Can't read version information from " + file.getAbsolutePath());
+                alert.showAndWait();
+            } else {
+                ObjectMapper mapper = new ObjectMapper();
+                try {
+                    currentVersion = mapper.readValue(file, SoftwareVersion.class);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
         return currentVersion;
@@ -70,7 +58,7 @@ public class SoftwareVersion implements Comparable<SoftwareVersion> {
         SoftwareVersion latestVersion = null;
         ObjectMapper mapper = new ObjectMapper();
         try {
-            latestVersion = mapper.readValue(new URL(URL), SoftwareVersion.class);
+            latestVersion = mapper.readValue(new URL(URL + "/software_" + getCurrentVersion().getChannel() + ".json"), SoftwareVersion.class);
         } catch (UnknownHostException|ConnectException e) {
             // exception can normally be thrown when there is no internet connectivity
         } catch (IOException e) {
@@ -79,16 +67,20 @@ public class SoftwareVersion implements Comparable<SoftwareVersion> {
         return Optional.ofNullable(latestVersion);
     }
 
+    private enum Channel {stable, nightly, internal}
+
     private String buildName;
     private String version;
+    private Channel channel;
     private String downloadURL;
     private Date releaseDate;
 
     @JsonCreator
-    private SoftwareVersion(@JsonProperty("build_name") String buildName, @JsonProperty("version") String version
+    private SoftwareVersion(@JsonProperty("build_name") String buildName, @JsonProperty("version") String version, @JsonProperty("channel") Channel channel
             , @JsonProperty("download_url") String downloadURL, @JsonProperty("release_date") java.util.Date releaseDate) {
         this.buildName = buildName;
         this.version = version;
+        this.channel = channel;
         this.downloadURL = downloadURL;
         this.releaseDate = releaseDate;
     }
@@ -99,6 +91,10 @@ public class SoftwareVersion implements Comparable<SoftwareVersion> {
 
     public String getVersionString() {
         return version;
+    }
+
+    public Channel getChannel() {
+        return channel;
     }
 
     public String getDownloadURL() {
