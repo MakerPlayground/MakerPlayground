e67b302691d1b46cad708e82e1e521fb4fb72414
kitngay <aak.kittipat@gmail.com>
2020-07-28 21:48:08 +0700

    fix add new project device and variable

--- src/main/java/io/makerplayground/project/DiagramClipboardDataDeserializer.java
+++ src/main/java/io/makerplayground/project/DiagramClipboardDataDeserializer.java
@@ -13,6 +13,7 @@ import javafx.collections.ObservableList;
 import org.w3c.dom.Node;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.*;
 
 public class DiagramClipboardDataDeserializer extends JsonDeserializer<DiagramClipboardData> {
@@ -36,10 +37,11 @@ public class DiagramClipboardDataDeserializer extends JsonDeserializer<DiagramCl
     @Override
     public DiagramClipboardData deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
         JsonNode root = mapper.readTree(jsonParser);
+        addNewVariable(root);
 
         List<ProjectDevice> deviceList = mapper.readValue(root.get("devices").traverse(),new TypeReference<List<ProjectDevice>>() {});
 
-        ArrayList<ProjectDevice> reNameDevices = addNewDevice(deviceList);
+        Map<ProjectDevice,ProjectDevice> reNameDevices = addNewDevice(deviceList);
         root = modifiedDeviceNameJson(root,reNameDevices);
 
         List<Scene> sceneList = mapper.readValue(root.get("scenes").traverse(),new TypeReference<List<Scene>>() {});
@@ -78,20 +80,23 @@ public class DiagramClipboardDataDeserializer extends JsonDeserializer<DiagramCl
     }
 
 
-    JsonNode modifiedDeviceNameJson(JsonNode jsonNode,  ArrayList<ProjectDevice> newDevices) {
-        for (ProjectDevice device : newDevices) {
+    JsonNode modifiedDeviceNameJson(JsonNode jsonNode,  Map<ProjectDevice,ProjectDevice> newDevices) {
+        for (ProjectDevice device : newDevices.keySet()) {
+
             for (JsonNode sceneNode : jsonNode.get("scenes") ) {
                 for (JsonNode settingNode : sceneNode.get("setting")) {
                     String name = settingNode.get("device").asText();
                     if(name.equalsIgnoreCase(device.getName())) {
-                        ((ObjectNode)settingNode).put("device", device.getName()+"_"+project.getProjectName().replace(" ","_"));
+                        System.out.println(newDevices.get(device).getName());
+                        ((ObjectNode)settingNode).put("device", newDevices.get(device).getName());
                     }
                     else if (name.equalsIgnoreCase("Memory")) {
                         for (JsonNode valueMap : settingNode.get("valueMap")) {
                             if (valueMap.get("type").asText().equalsIgnoreCase("CustomNumberExpression")) {
                                 for (JsonNode terms : valueMap.get("value").get("terms")) {
                                     if(terms.get("value").get("name").asText().equalsIgnoreCase(device.getName())){
-                                        ((ObjectNode)terms.get("value")).put("name", device.getName()+"_"+project.getProjectName().replace(" ","_"));
+                                        System.out.println(newDevices.get(device).getName());
+                                        ((ObjectNode)terms.get("value")).put("name", newDevices.get(device).getName());
                                     }
                                 }
                             }
@@ -103,7 +108,8 @@ public class DiagramClipboardDataDeserializer extends JsonDeserializer<DiagramCl
                 for (JsonNode settingNode : sceneNode.get("setting")) {
                     String name = settingNode.get("device").asText();
                     if (name.equalsIgnoreCase(device.getName())) {
-                        ((ObjectNode)settingNode).put("device", device.getName()+"_"+project.getProjectName().replace(" ","_"));
+                        System.out.println(newDevices.get(device).getName());
+                        ((ObjectNode)settingNode).put("device", newDevices.get(device).getName());
                     }
                 }
             }
@@ -111,26 +117,36 @@ public class DiagramClipboardDataDeserializer extends JsonDeserializer<DiagramCl
         return jsonNode;
     }
 
-    ArrayList<ProjectDevice> addNewDevice(List<ProjectDevice> deviceList) {
-        ArrayList<ProjectDevice> devices = new  ArrayList<ProjectDevice>();
+    Map<ProjectDevice,ProjectDevice> addNewDevice(List<ProjectDevice> deviceList) {
         ObservableList<ProjectDevice> projectDeviceSet = project.getUnmodifiableProjectDevice();
+        Map<ProjectDevice,ProjectDevice> projectDeviceMap = new HashMap<>();
         if (projectDeviceSet.isEmpty()) {
             for (ProjectDevice projectDevice : deviceList) {
                 ProjectDevice newProjectDevice = new ProjectDevice(projectDevice.getName(),projectDevice.getGenericDevice());
                 project.addDevice(newProjectDevice);
             }
         } else {
+            List<String> projectDeviceName = new ArrayList<>();
+            for (ProjectDevice projectDevice : projectDeviceSet) {
+                projectDeviceName.add(projectDevice.getName());
+            }
+
             for (ProjectDevice projectDevice : deviceList) {
-                if (!projectDeviceSet.contains(projectDevice)) {
-                    System.out.println(projectDevice.getGenericDevice().getName());
+                if (!projectDeviceName.contains(projectDevice.getName())) {
                     ProjectDevice newProjectDevice = new ProjectDevice(projectDevice.getName(),projectDevice.getGenericDevice());
                     project.addDevice(newProjectDevice);
                 } else {
                     for(ProjectDevice pr : projectDeviceSet) {
                         if(projectDevice.getName().equalsIgnoreCase(pr.getName())) {
                             if(!projectDevice.getGenericDevice().getName().equalsIgnoreCase(pr.getGenericDevice().getName())) {
-                                devices.add(projectDevice);
-                                String name = projectDevice.getName()+"_"+project.getProjectName().replace(" ","_");
+                                String name = projectDevice.getName()+project.getProjectName();
+                                int occurrences = Collections.frequency(projectDeviceName, name);
+                                while (projectDeviceName.contains(name+"_("+(occurrences)+")")) {
+                                    occurrences ++;
+                                }
+                                name = name+"_("+(occurrences)+")";
+                                projectDevice.setName(name);
+                                projectDeviceMap.put(pr,projectDevice);
                                 ProjectDevice newProjectDevice = new ProjectDevice(name,projectDevice.getGenericDevice());
                                 project.addDevice(newProjectDevice);
                             }
@@ -139,6 +155,15 @@ public class DiagramClipboardDataDeserializer extends JsonDeserializer<DiagramCl
                 }
             }
         }
-        return devices;
+
+        return projectDeviceMap;
+    }
+
+    void addNewVariable(JsonNode node){
+        for(JsonNode variable : node.get("variable")) {
+            if(project.getVariableByName(variable.asText()).isEmpty()){
+                project.addVariable(variable.asText());
+            }
+        }
     }
 }
